dddfe633d5dd2973f7df0b7bf4a48175ab2cc344
Gio Formichella <gioformichella@gmail.com>
2024-07-02 18:32:59 +0200

    Implemented interpolation method

--- representation_change.py
+++ representation_change.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy.linalg import inv
 
 
+# Evaluating the polynomial for n distinct points achieves the point representation of the same polynomial.
 def evaluation(pol: np.array, x):
     """
-    Using Horner's rule for polynomial evaluation
+    Using Horner's rule for polynomial evaluation. Complexity: O(n)
     :param pol: polynomial in coefficient form as numpy array
     :param x: point of evaluation
     :return: evaluation of pol at x
@@ -23,3 +25,22 @@ def evaluation(pol: np.array, x):
 
     s += pol[i]
     return s
+
+
+# Note: a O(n^2) algorithm exists that uses Lagrange's formula, I wasn't able to find a way to simply implement it
+def interpolation(pol: list) -> np.array:
+    """
+    Complexity: O(n^3)
+    :param pol: point representation of the polynomial
+    :return: coefficient representation of the same polynomial
+    """
+    if not pol:
+        raise ValueError("No points given")
+
+    x = [point[0] for point in pol]
+    y = [point[1] for point in pol]
+    y = np.array(y).reshape(-1, 1)  # column vector
+    v = np.vander(x)
+
+    a = np.dot(inv(v), y).reshape(1, -1)
+    return a
--- test/test_representation_change.py
+++ test/test_representation_change.py
@@ -1,5 +1,5 @@
 import unittest
-
+import numpy.testing as npt
 from representation_change import *
 
 
@@ -15,10 +15,25 @@ class MyTestCase(unittest.TestCase):
         pol3 = np.array([1, 1, 1])
         self.assertEqual(evaluation(pol3, 0), 1)
         self.assertEqual(evaluation(pol3, 1), 3)
+        self.assertEqual(evaluation(pol3, 1j), 1j)
 
         # complex coefficient polynomial
         pol4 = np.array([1 + 1j, 1j])
         self.assertEqual(evaluation(pol4, 2), 2 + 3j)
+
+    def test_interpolation(self):
+        # testing edge cases
+        self.assertRaises(ValueError, interpolation, [])
+
+        # real points
+        pol1 = [(1, 3), (2, 7), (3, 13)]
+        r1 = np.array([1.0, 1.0, 1.0]).reshape(1, 3)
+        npt.assert_almost_equal(interpolation(pol1), r1)
+
+        # complex points
+        pol2 = [(1, 1j), (2j, 2)]
+        r2 = np.array([(-4 - 3j)/5, (4 + 8j)/5]).reshape(1, 2)
+        npt.assert_almost_equal(interpolation(pol2), r2)
 
 
 if __name__ == '__main__':
