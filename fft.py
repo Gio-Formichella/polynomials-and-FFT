dca32e04561a38ca48a31c5d352c45677b5f9c3f
Gio Formichella <gioformichella@gmail.com>
2024-07-15 17:00:46 +0200

    Made recursive_fft() also able to calculate ifft

--- fft.py
+++ fft.py
@@ -1,34 +1,41 @@
 import numpy as np
 
 
-def recursive_fft(a: np.array):
+def recursive_fft(a: np.array, inverse=False, root=True) -> np.array:
     """
-    Fast Fourier Transformation of polynomial
+    Fast Fourier Transformation of polynomial, can be used to calculate both the direct and inverse DFT.
+    Complexity: O(n lg(n))
 
-    Note: length of a must be a power of 2, 0 in front can be added to solve constraint
+    Note: length of a must be a power of 2, to solve constraint add 0 in front of a
     :param a: polynomial
-    :return: DFT(a)
+    :param inverse: direct or inverse DFT flag
+    :param root: used to check for root function in recursive calls
+    :return: DFT(a) in point form
     """
     n = len(a)
 
     if n == 1:
         return a
 
-    # n must be a power of 2
-    half_size = int(n / 2)
+    half_size = n // 2
 
-    omega_n = np.exp(2 * np.pi * 1j / n)
+    omega_n = np.exp(2 * np.pi * 1j / n)  # twiddle factor
+    if inverse:
+        omega_n **= -1
+
     omega = 1
 
-    even_a = a[1::2]
-    odd_a = a[::2]
-    even_y = recursive_fft(even_a)
-    odd_y = recursive_fft(odd_a)
+    even_a = a[0::2]
+    odd_a = a[1::2]
+    even_y = recursive_fft(even_a, inverse, False)
+    odd_y = recursive_fft(odd_a, inverse, False)
 
-    y = np.empty(n)
+    y = np.empty(n, dtype=complex)
     for k in range(half_size):
-        y[k] = even_y[k] + odd_y[k]
+        y[k] = even_y[k] + omega * odd_y[k]
         y[k + half_size] = even_y[k] - omega * odd_y[k]
         omega = omega * omega_n
 
+    if inverse and root:
+        y /= n
     return y
--- test/test_fft.py
+++ test/test_fft.py
@@ -1,13 +1,26 @@
 import unittest
-from fft import *
+
 import numpy.testing as npt
+
+from fft import *
 
 
 class MyTestCase(unittest.TestCase):
     def test_recursive_fft(self):
+        # direct fft
         pol1 = np.array([1, 1, 1, 1])  # n = 4 = 2^2
         r = np.array([4, 0, 0, 0])
         npt.assert_almost_equal(recursive_fft(pol1), r)
+
+        # inverse fft
+        points = np.array([4, 0, 0, 0])
+        r = np.array([1, 1, 1, 1])
+        npt.assert_almost_equal(recursive_fft(points, True), r)
+
+        #
+        pol2 = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+        points = recursive_fft(pol2)
+        npt.assert_almost_equal(pol2, recursive_fft(points, True))
 
 
 if __name__ == '__main__':
